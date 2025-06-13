75801ec6085f8b9e34835bcd74aa1e2a8f67ce63
liukeqqs <364923792@gmail.com>
2025-06-14 04:46:17 +0800

    Revert "测试1"
    This reverts commit 14818ece9ae6976c30634da84d22f341af78a7a5.

--- handler/socks/v5/connect.go
+++ handler/socks/v5/connect.go
@@ -73,16 +73,16 @@ func (h *socks5Handler) handleConnect(ctx context.Context, conn net.Conn, networ
 	}
 	t := time.Now()
 	log.Infof("%s <-> %s", conn.RemoteAddr(), address)
-	netpkg.Transport(conn, cc)
+
 	//netpkg.Transport1(rw, cc, address, string(ctxvalue.SidFromContext(ctx)))
 		// 使用 TransportWithStats 替代 Transport1
-/*     	netpkg.TransportWithStats(
+    	netpkg.TransportWithStats(
     		rw,          // 客户端连接
     		cc,          // 目标服务器连接
     		address,     // 目标地址（如 example.com:443）
     		string(ctxvalue.SidFromContext(ctx)), // 会话ID
     		localPort,   // 代理本地端口（如 1080）
-    	) */
+    	)
 
 	log.WithFields(map[string]any{
 		"duration": time.Since(t),
